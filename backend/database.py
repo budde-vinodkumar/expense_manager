a697934da1815691829975449f679f79840a4ecf
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-27 20:18:05 +0530

    intial commit

---
+++ backend/database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect("expense.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_tables():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        amount REAL,
+        category TEXT,
+        date TEXT,
+        description TEXT,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+    """)
+
+    conn.commit()
+    conn.close()
