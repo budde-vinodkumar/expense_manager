1a96b227344ffc2f4a8cfb12f2b5b208db87276d
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-29 19:52:45 +0530

    expence file

--- backend/expence.py
+++ backend/expence.py
@@ -2,19 +2,21 @@ from flask import request, redirect, session
 from database import get_db_connection
 
 def add_expense():
+    if "user_id" not in session:
+        return redirect("/")
+
     amount = request.form["amount"]
     category = request.form["category"]
     date = request.form["date"]
     description = request.form["description"]
-    user_id = session["user_id"]
 
     conn = get_db_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
-    INSERT INTO expenses (user_id, amount, category, date, description)
-    VALUES (?, ?, ?, ?, ?)
-    """, (user_id, amount, category, date, description))
+        INSERT INTO expenses (user_id, amount, category, date, description)
+        VALUES (?, ?, ?, ?, ?)
+    """, (session["user_id"], amount, category, date, description))
 
     conn.commit()
     conn.close()
@@ -23,7 +25,7 @@ def add_expense():
 def get_expenses(user_id):
     conn = get_db_connection()
     expenses = conn.execute(
-        "SELECT * FROM expenses WHERE user_id=?",
+        "SELECT * FROM expenses WHERE user_id = ?",
         (user_id,)
     ).fetchall()
     conn.close()
