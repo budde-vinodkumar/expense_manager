a7510795ef1bae7c9fb4557975c6bb3dc32b0f21
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-03 23:06:52 +0530

    auth file

--- backend/auth.py
+++ backend/auth.py
@@ -1,6 +1,10 @@
 from flask import request, redirect, render_template, session
+from werkzeug.security import generate_password_hash, check_password_hash
 from database import get_db_connection
 
+# --------------------
+# REGISTER
+# --------------------
 def register_user():
     username = request.form.get("username")
     password = request.form.get("password")
@@ -11,24 +15,30 @@ def register_user():
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    user = cursor.execute(
-        "SELECT * FROM users WHERE username = ?",
+    existing_user = cursor.execute(
+        "SELECT id FROM users WHERE username=?",
         (username,)
     ).fetchone()
 
-    if user:
+    if existing_user:
         conn.close()
         return render_template("register.html", error="Username already exists")
 
+    hashed_password = generate_password_hash(password)
+
     cursor.execute(
         "INSERT INTO users (username, password) VALUES (?, ?)",
-        (username, password)
+        (username, hashed_password)
     )
 
     conn.commit()
     conn.close()
     return redirect("/")
 
+
+# --------------------
+# LOGIN
+# --------------------
 def login_user():
     username = request.form.get("username")
     password = request.form.get("password")
@@ -37,17 +47,14 @@ def login_user():
     cursor = conn.cursor()
 
     user = cursor.execute(
-        "SELECT * FROM users WHERE username = ? AND password = ?",
-        (username, password)
+        "SELECT * FROM users WHERE username=?",
+        (username,)
     ).fetchone()
-
-    print("LOGIN ROUTE HIT")
-
 
     conn.close()
 
-    if user:
+    if user and check_password_hash(user["password"], password):
         session["user_id"] = user["id"]
         return redirect("/dashboard")
 
-    return render_template("login.html", error="Invalid login")
+    return render_template("login.html", error="Invalid username or password")
