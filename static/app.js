64a9650cde68c8ccc78f25349e1031fd6c7bb90c
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-19 22:07:42 +0530

    changes

--- static/app.js
+++ static/app.js
@@ -16,3 +16,11 @@ function handleSubmit(btnId, formId) {
     btn.innerText = "Creating Account...";
   });
 }
+
+function handleSubmit(btnId, formId) {
+  const btn = document.getElementById(btnId);
+  const form = document.getElementById(formId); 
+  caches.keys().then(function (cacheNames) {
+    cacheNames.forEach(function (cacheName) {
+      caches.delete(cacheName);
+    });
